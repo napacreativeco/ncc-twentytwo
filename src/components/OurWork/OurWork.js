8c9f993294f2644213dc4dc4dbc5459b3031fb57
James Rogers <hello@napacreativeco.com>
2022-05-29 14:35:28 -0700

    catching up

--- src/components/OurWork/OurWork.js
+++ src/components/OurWork/OurWork.js
@@ -2,22 +2,29 @@ import WebDev from "./WebDev";
 import WebDesign from "./WebDesign";
 import Production from "./Production";
 import './_ourwork.scss';
+import { useState } from 'react';
 
 function OurWork() {
+    const [isvisible, setVisible] = useState('false');
+
+    const design = document.getElementById('webdesign');
+    const dev = document.getElementById('webdev');
+    const prod = document.getElementById('production');
+
     return (
-        <section>
+        <section className="our-work-section">
             <div className="work_slides_controller">
                 <ul>
-                    <li>Design</li>
-                    <li>Development</li>
-                    <li>Production</li>
+                    <li onClick={ () => {} }>Design</li>
+                    <li onClick={ () => {} }>Development</li>
+                    <li onClick={ () => {} }>Production</li>
                 </ul>
                 <div>&nbsp;</div>
             </div>
             <div className="work_slides_container">
-                <div className="work_slide" id="s-webdev"><WebDev /></div>
-                <div className="work_slide" id="s-webdesign"><WebDesign /></div>
-                <div className="work_slide" id="s-production"><Production /></div>
+                <div className="work_slide" data-visible={isvisible} id="webdev"><WebDev /></div>
+                <div className="work_slide" data-visible={isvisible} id="webdesign"><WebDesign /></div>
+                <div className="work_slide" data-visible={isvisible} id="production"><Production /></div>
             </div>
         </section>
     );
